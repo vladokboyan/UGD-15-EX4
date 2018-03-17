65f9528dd04cc56d5d9d3f1d10c31f5e4a160fae
victor-myha <victormyhaletsky@gmail.com>
2018-03-17 14:49:43 +0200

    first commit

---
+++ Assets/skripts/PlayerContloreller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContloreller : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();   	
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float moveHorizonantal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizonantal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);    	
+    }
+}
